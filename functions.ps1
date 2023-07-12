18ee487e8dce4b1f378811edf6601f1edd95d156
vonbir <135559506+vonbir@users.noreply.github.com>
2023-07-12 15:22:00 -0400

    Update functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -1,5 +1,4 @@
 
-
 Function Get-ADG {
     [CmdletBinding()]
     Param(
@@ -54,22 +53,20 @@ Function Verify-UserExist {
         [Parameter(Mandatory = $true)]
         [string]$UserPrincipalName
     )
-
     try {
-        $user = Get-MsolUser -SearchString $UserPrincipalName -ErrorAction Stop | Select-Object DisplayName, Title, Department, City, Office, StreetAddress, State, PhoneNumber, isLicensed, Licenses, ObjectID, userPrincipalName, UserType, BlockCredential
+        $user = Get-MsolUser -SearchString $UserPrincipalName | Select-Object DisplayName, Title, Department, City, Office, StreetAddress, State, PhoneNumber, isLicensed, Licenses, ObjectID, userPrincipalName, UserType, BlockCredential
 
         $groups = Get-AzureADUserMembership -ObjectId $user.ObjectId | Where-Object { $_.ObjectType -eq "Group" } | Select-Object DisplayName, Description
 
-
         Write-Host
-        Write-Host "USER DETAILS:" -ForegroundColor Yellow
+        Write-Host "USER DETAILS:" -ForegroundColor Yellow -BackgroundColor DarkRed
         Write-Host
-        $user | Format-List | Out-String -Width 4096 | ForEach-Object { $_.Trim() }
+        $user | Out-String -Width 4096 | ForEach-Object { $_.Trim() }
         Write-Host
-        Write-Host "THIS USER BELONGS TO THESE AZURE GROUPS:" -ForegroundColor Yellow
-        $groups | Format-Table -AutoSize | Out-String -Width 4096
-
-    } catch {
+        Write-Host "THIS USER BELONGS TO THESE AZURE GROUPS:" -ForegroundColor Yellow -BackgroundColor DarkRed
+        $groups | Format-Table -AutoSize| Out-String -Width 4096
+    } 
+    catch {
         Write-Host "Username does not exist, please try again." -ForegroundColor Yellow
     }
 }
@@ -150,5 +147,77 @@ Function Verify-UserTerminated {
         $results | Format-List *
     } Catch {
         Write-Host "The user does not exist. Please try again later.." -ForegroundColor Red -BackgroundColor Yellow
+    }
+}
+
+
+Function Create-ADComputer {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [ValidateSet("GGNB", "FGNB", "HTNB", "THNB", "GBSNB")]
+        [string]$CompanyGroup
+    )
+
+    $names = Get-ADComputer -Filter * -SearchBase "OU=Sites and Divisions,OU=Great Gulf Group,DC=greatgulf,DC=biz" |
+        Where-Object { $_.Name -match "^$CompanyGroup\d+$" } |
+        Select-Object -ExpandProperty Name
+
+    [int[]]$num = $names -replace '\D+'
+
+    if ($CompanyGroup -eq "HTNB"){
+    $latestNumber = ($num | Where-Object { $_ -ne "1463" -and $_ -ne "225" } | Sort-Object -Descending | Select-Object -First 1) -as [int]
+    $nextNumber = $latestNumber + 1
+    $nextComputerName = "$CompanyGroup{00:D3}" -f $nextNumber
+    }
+    else{
+    $latestNumber = ($num | Sort-Object -Descending | Select-Object -First 1) -as [int]
+    $nextNumber = $latestNumber + 1
+    $nextComputerName = "$CompanyGroup{0:D4}" -f $nextNumber
+    }
+    Write-Host
+    Write-Host "Next available computer name:" -ForegroundColor Cyan -NoNewline; Write-Host " $nextComputerName" -ForegroundColor Yellow;
+    Write-Host
+
+    $createComputer = Read-Host "Do you want to create this computer object? (Y/N)"
+
+    if ($createComputer -eq "Y"){
+    # asks for the on-prem AD admin credentials to verify and create the computer object
+    Write-Host
+    if (-not ($adminuser)){
+    [string]$adminuser = Read-Host "Please enter your on-prem AD username to verify" 
+    $adminpass = Read-Host "Please enter your on-prem AD password" -AsSecureString 
+    $Cred = New-Object System.Management.Automation.PSCredential $adminuser,$adminpass
+    }
+    
+    Write-Host
+    $displayuser = Read-Host "Who will this computer belong to? (username for description)"
+
+    if ($CompanyGroup -eq "THNB") {
+        $computerPath = "OU=Computers,OU=TuckerHiRise,OU=Sites and Divisions,OU=Great Gulf Group,DC=greatgulf,DC=biz"
+        }
+        elseif ($CompanyGroup -eq "GGNB"){
+        $computerPath = "OU=Computers,OU=GreatGulf,OU=Sites and Divisions,OU=Great Gulf Group,DC=greatgulf,DC=biz"
+        }
+        elseif ($CompanyGroup -eq "HTNB"){
+        $computerPath = "OU=Computers,OU=BrockportHS,OU=Sites and Divisions,OU=Great Gulf Group,DC=greatgulf,DC=biz"
+        }
+        elseif ($CompanyGroup -eq "GBSNB"){
+        $computerPath = "OU=Computers,OU=GreatBuilderSolutions,OU=Sites and Divisions,OU=Great Gulf Group,DC=greatgulf,DC=biz"
+        }
+  else {
+        Write-Host
+        Write-Host "Computer object creation cancelled...." -ForegroundColor Red
+        Write-Host
+        }
+  $computerProperties = @{
+            Name = $nextComputerName
+            SamAccountName = $nextComputerName
+            Path = $computerPath
+            Enabled = $true
+            Description = $displayuser
+        }
+        New-ADComputer @computerProperties -PassThru -Credential $Cred
+        Write-Host "Computer object created successfully!!!" -ForegroundColor Yellow
     }
 }
