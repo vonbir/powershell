84a322d870e0ad2f84350ee758b231618578cbd6
vonbir <135559506+vonbir@users.noreply.github.com>
2023-07-13 16:08:08 -0400

    o365_services.ps1

--- o365_services.ps1
+++ o365_services.ps1
@@ -1,5 +1,7 @@
-﻿
+
 # Check if modules are installed, and install if necessary
+
+$ErrorActionPreference = "SilentlyContinue"
 $requiredModules = @("AzureAD", "MSOnline", "ExchangeOnlineManagement")
 
 foreach ($module in $requiredModules) {
@@ -9,28 +11,33 @@ foreach ($module in $requiredModules) {
     }
 }
 
+$Color = @{Foregroundcolor = 'Yellow'; Backgroundcolor = 'Black'}
+
 # Connect to Azure AD
 if (-not (Get-AzureADCurrentSessionInfo)) {
-    Write-Host "Connecting to Azure AD"
+    Write-Host "Connecting to Azure AD....." @Color
     Connect-AzureAD
+     Write-Host "Already connected to Azure AD......" @Color
 } else {
-    Write-Host "Already connected to Azure AD"
+    Write-Host "Already connected to Azure AD......" @Color
 }
 
 # Connect to Azure AD using the MSOnline module
 if (-not (Get-MsolAccountSku)) {
-    Write-Host "Connecting to Azure AD using MSOnline module"
+    Write-Host "Connecting to Azure AD using MSOnline module....." @Color
     Connect-MsolService
+    Write-Host "Already connected to Azure AD using MSOnline module......." @Color
 } else {
-    Write-Host "Already connected to Azure AD using MSOnline module"
+    Write-Host "Already connected to Azure AD using MSOnline module......." @Color
 }
 
 # Connect to Exchange Online
 if (-not (Get-PSSession | Select-Object -Property Name -First 1 | Where-Object { $_.Name -like "ExchangeOnline*" })) {
-    Write-Host "Connecting to Exchange Online"
+    Write-Host "Connecting to Exchange Online....." @Color
     Connect-ExchangeOnline
+    Write-Host "Already connected to Exchange Online......." @Color
 } else {
-    Write-Host "Already connected to Exchange Online"
+    Write-Host "Already connected to Exchange Online......." @Color
 }
 
 <# Connect to SharePoint Online
